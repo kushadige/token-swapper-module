f12e43c8b873cff8f893f8de16b75d316ecc58b9
kushadige <oguzhankuslar@gmail.com>
2024-08-01 09:00:52 +0300

    chore: update tailwind.config.ts with color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,33 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config = {
   darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
-  prefix: "",
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
+      colors: {
+        patara_black: "#0C0C14",
+        patara_black_50: "#282828",
+        patara_black_100: "#444444",
+        patara_dark_blue: "#001229",
+        patara_blue: "#006EFF",
+        patara_blue_50: "#CCE2FF",
+        patara_blue_400: "#197CFF",
+        patara_blue_500: "#4DA2FF",
+        patara_blue_600: "#2775CA",
+        patara_gray_50: "#F5F5F5",
+        patara_gray_75: "#EBEBF5",
+        patara_gray_100: "#DCDCE6",
+        patara_gray_200: "#C8C8DC",
+        patara_gray_300: "#B4B4BE",
+        patara_gray_400: "#A0A0A8",
+        patara_gray_500: "#8C8C8F",
+        patara_gray_600: "#787882",
+        patara_gray_700: "#404048",
+        patara_gray_800: "#343330",
+        patara_red: "#FF4D4D",
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -34,7 +44,19 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+  plugins: [
+    require("tailwindcss-animate"),
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".absolute-center": {
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        },
+      });
+    }),
+  ],
+} satisfies Config;
 
-export default config
+export default config;
